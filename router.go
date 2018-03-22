0c6f62217013b8352713b277cd5082ad72d11592
Christopher Fox <cfox1290@gmail.com>
2018-03-21 23:21:38 -0600

    server finished

--- router.go
+++ router.go
@@ -1,10 +1,12 @@
 package main
 
-import(
+import (
 	"fmt"
+	r "github.com/dancannon/gorethink"
 	"github.com/gorilla/websocket"
 	"net/http"
 )
+
 type Handler func(*Client, interface{})
 
 var upgrader = websocket.Upgrader{
@@ -12,32 +14,38 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
+
 type Router struct {
-	rules map[string]Handler
+	rules   map[string]Handler
+	session *r.Session
 }
-func NewRouter() *Router{
+
+func NewRouter(session *r.Session) *Router {
 	return &Router{
-		rules: make(map[string]Handler),
+		rules:   make(map[string]Handler),
+		session: session,
 	}
 }
-func (r *Router) Handle(msgName string, handler Handler){
+
+func (r *Router) Handle(msgName string, handler Handler) {
 	r.rules[msgName] = handler
 }
 
-func (r *Router) FindHandler(msgName string) (Handler, bool){
+func (r *Router) FindHandler(msgName string) (Handler, bool) {
 	handler, found := r.rules[msgName]
 	return handler, found
 }
 
-
-func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
+func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	socket, err := upgrader.Upgrade(w, r, nil)
-	if err != nil{
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err.Error())
 		return
 	}
-	client := NewClient(socket, e.FindHandler)
+	client := NewClient(socket, e.FindHandler, e.session)
+	defer client.Close()
 	go client.Write()
 	client.Read()
+
 }
